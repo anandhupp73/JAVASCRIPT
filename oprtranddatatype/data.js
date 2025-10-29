a90ac8858110bd65dafc0ffd154d8a5ee0dd5348
anandhupp73 <anandhakrishnanpp73@gmail.com>
2025-10-29 12:57:46 +0530

    operators

---
+++ oprtranddatatype/data.js
@@ -0,0 +1,37 @@
+obj={'name':'aswin'}
+console.log(obj.name)
+
+console.log(typeof obj)
+
+arr=[1,2,3,'anandhu',4]
+console.log(arr)
+console.log(typeof arr)
+
+num = '5'
+console.log(+num)
+console.log(-num)
+
+num2 = 5 + '5'
+console.log(num2)
+
+age = 18
+canVote = (age>18)?'yes':'no'
+console.log(canVote)
+
+num = 5
+num++
+console.log(num)
+num--
+console.log(num)
+
+assign = 10
+assign += 10
+console.log(assign)
+
+num = 7
+numtest = ( num%2 == 1)?'odd':'even'
+console.log(numtest)
+
+num = 287
+numdiv = ( num%3 == 0)?'divisible':'not'
+console.log(numdiv)
