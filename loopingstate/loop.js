48540adc6b37cc531a91e9d672489c3396fe2f5c
anandhupp73 <anandhakrishnanpp73@gmail.com>
2025-10-31 13:18:20 +0530

    loops

---
+++ loopingstate/loop.js
@@ -0,0 +1,50 @@
+// for loop 
+
+// for (initialization;condition;increament/decrement){
+//     staement
+// }
+
+for ( i=1;i<=10;i++){
+    console.log(i)
+}
+
+console.log('reverse')
+
+for (i=10;i>=1;i--){
+    console.log(i)
+}
+
+for (i=1;i<=3;i++){
+    row = '';
+    for (j=1;j<=3;j++){
+        row += '*';
+    }
+    console.log(row)
+}
+
+
+console.log('for even')
+
+nums = 1
+nume = 20
+for (i=nums;i<=nume;i++)
+    if (i%2==0){
+        console.log(i)
+    }
+            
+
+console.log('while loop')
+
+i=1
+while (i<=10) {
+    console.log(i)
+    i++
+}
+
+console.log('do while')
+
+i=11
+do{
+    console.log(i)
+    i++
+}while(i<=10)
---
+++ loopingstate/ques.js
@@ -0,0 +1,186 @@
+// upto 100 
+
+// for (i=1;i<=100;i++){
+//     console.log(i)
+// }
+
+// i=1
+// while(i<=100){
+//     console.log(i)
+//     i++
+// }
+
+// i=1
+// do{
+//     console.log(i)
+//     i++
+// }while(i<=100)
+
+// even numbers 
+
+// for (i=1;i<=50;i++){
+//     if (i%2 == 0){
+//         console.log(i)
+//     }
+// }
+
+// i=1
+// while(i<=50){
+//     if (i%2 == 0){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+// i=1
+// do{
+//     if (i%2 == 0){
+//         console.log(i)
+//     }
+//     i++
+// }while(i<=50)
+
+// sum upto n 
+
+// sum = 0
+// for (i=1;i<=10;i++){
+//     sum += i
+// }
+// console.log(sum)
+
+// i=1
+// sum=0
+// while(i<=10){
+//     sum += i
+//     i++
+// } 
+// console.log(sum)
+
+// i=1
+// sum=0
+// do{
+//     sum += i
+//     i++
+// }while(i<=10)
+//     console.log(sum)
+
+
+// sqaure of num 
+
+// for (i=1;i<=10;i++){
+//     sqr = i*i
+//     console.log(sqr)
+// }
+
+// i=1
+// while(i<=10){
+//     sqr = i*i
+//     console.log(i  + ' sqr = '+  sqr)
+//     i++
+// }
+
+// i=1
+// do{
+//     sqr = i*i
+//     console.log(i + ' sqr = ' + sqr)
+//     i++
+// }while(i<=10)
+
+// multi 
+
+// num=6
+// for (i=1;i<=10;i++){
+//     console.log(num + ' * ' + i + ' = ' + i*num)
+// }
+
+// i=1
+// num=5
+// while(i<=10){
+//     console.log(num + ' * ' + i + ' = ' + i*num)
+//     i++
+// }
+
+// i=1
+// num = 3
+// do {
+//     console.log(num + ' * ' + i  + " = " + i*num)
+//     i++
+// }while(i<=10)
+
+// even and odd num count 
+
+// counte = 0
+// counto = 0
+// for (i=1;i<=20;i++){
+//     if (i%2 == 0){
+//         counte += 1
+//     }
+//     if (i%2 == 1){
+//         counto += 1
+//     }
+// }
+// console.log('even num count ')
+// console.log(counte)
+// console.log('odd number count')
+// console.log(counto)
+
+// counte = 0
+// counto = 0
+// i=1
+// while(i<=25){
+//     if(i%2 == 0){
+//         counte += 1
+//     }
+//     if(i%2 == 1){
+//         counto += 1
+//     }
+//     i++
+// }
+// console.log('even num count ')
+// console.log(counte)
+// console.log('odd number count')
+// console.log(counto)
+
+// counte = 0
+// counto = 0
+// i=1
+// do{
+//     if(i%2 == 0){
+//         counte += 1
+//     }
+//     if(i%2 == 1){
+//         counto += 1
+//     }
+//     i++
+// }while(i<=20)
+//     console.log('even num count')
+//     console.log(counte)
+//     console.log('odd num count')
+//     console.log(counto)
+
+// factorial 
+
+// num = 6
+// fact = 1
+// for (i=1;i<=num;i++){
+//     fact = fact*i
+// }
+// console.log(fact)
+
+// i=1
+// num = 5
+// fact = 1
+// while(i<=num){
+//     fact = fact * i
+//     i++
+// }
+// console.log(fact)
+
+i=1
+num=7
+fact=1
+do{
+    fact = fact * i
+    i++
+}while(i<=num)
+    console.log(fact)
