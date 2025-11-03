282b621c917bb3670b2b2a6f930466c969a30dae
anandhupp73 <anandhakrishnanpp73@gmail.com>
2025-11-03 21:43:50 +0530

    function

--- functions/func.js
+++ functions/func.js
@@ -3,29 +3,58 @@
 // }
 // greet()
 
-function num(num1,num2){
-    console.log(num1,num2)
-}
-num(4,5)
+// function num(num1,num2){
+//     console.log(num1,num2)
+// }
+// num(4,5)
 
-function num(num1,num2){
-    let result = num1+num2
-    return result
-}
+// function num(num1,num2){
+//     let result = num1+num2
+//     return result
+// }
 
-r=num(4,5)
-console.log(r)
+// r=num(4,5)
+// console.log(r)
 
-// anonymous function 
+// // anonymous function 
 
-let greet = function(){
-    console.log('good morning')
-}
-greet()
-// console.log(greet)
+// let greet = function(){
+//     console.log('good morning')
+// }
+// greet()
+// // console.log(greet)
 
 
 
-const add=(a,b)=> a+b
-console.log(add(5,5))
+// const add=(a,b)=> a+b
+// console.log(add(5,5))
 
+
+// function fact(num){
+//     let fact=1
+//         for(i=1;i<=num;i++){
+//             fact = fact * i
+//         }
+//         return fact
+// }
+// fact = fact(5)
+// console.log(fact)
+
+// function range(num){
+//     if (num >= 1 && num <= 11){
+//         console.log('found')
+//     }
+//     else{
+//         console.log('not found')
+//     }
+// }
+// range(15)
+
+// function sum(x,y){
+//     let s = x + y
+//     let sub = x - y
+//     let p = x * y
+//     return {s,sub,p}
+// }
+// const {s,sub,p}=sum(8,2)
+// console.log('sum = '+ s + ' sub = ' + sub + ' product = ' + p)
