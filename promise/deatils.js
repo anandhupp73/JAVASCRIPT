437d9d8ecc80d3cf569723198a9d826eb2495a53
anandhupp73 <anandhakrishnanpp73@gmail.com>
2025-11-27 19:42:24 +0530

    todo

--- promise/deatils.js
+++ promise/deatils.js
@@ -1,25 +1,27 @@
 const params = new URLSearchParams(location.search);
 const id = params.get("id")
-
 function getDetails(id){
     fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
-    .then((res)=>res.json())
-    .then((data)=>{
-        console.log(data)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
 
-        let str = `<table border="1">
-            <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>status</th>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-            </tr>
-        </table>`
-    })
+            let str = `
+            <table border="1">
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Status</th>
+                </tr>
+                <tr>
+                    <td>${data.id}</td>
+                    <td>${data.title}</td>
+                    <td style="color:${data.completed ? "green" : "red"};font-size:18px;">${data.completed}</td>
+                </tr>
+            </table>`;
+
+            document.querySelector(".data").innerHTML = str;
+        });
 }
 
-getDetails()
+getDetails(id);
