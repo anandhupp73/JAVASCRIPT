01c2f7f67f5a08ff1e8d4e26c6c9f1e53c2369da
anandhupp73 <anandhakrishnanpp73@gmail.com>
2025-11-30 19:37:59 +0530

    search

--- products/prod.js
+++ products/prod.js
@@ -1,33 +1,81 @@
-const products = []
+// const products = []
+// async function GetProducts() {
+//     try{
+//         const res = await fetch('https://dummyjson.com/products')
+//         const prod = await res.json()
+//         console.log(prod)
+
+//         let str = ``
+//         prod.products.forEach(product => {
+            
+            // str+= `<a href="./proddetails.html?id=${product.id}">
+            //     <div class="card">
+            //         <img src="${product.thumbnail}" alt="img">
+            //         <div class="title">
+            //             ${product.title}
+            //         </div>
+            //         <div class="price">
+            //             $${product.price}
+            //         </div>
+            //     </div>
+            //     </a>`
+//             console.log(product)
+
+//         });
+//         document.querySelector('.cards').innerHTML = str
+
+//     }
+//     catch (error){
+//         console.log(`error found ${error}`)
+//     }
+// }
+
+// GetProducts()
+
+const products = [];
+
 async function GetProducts() {
     try{
-        const res = await fetch('https://dummyjson.com/products')
-        const prod = await res.json()
-        console.log(prod)
+        const res = await fetch('https://dummyjson.com/products');
+        const prod = await res.json();
 
-        let str = ``
-        prod.products.forEach(product => {
-            
-            str+= `<a href="./proddetails.html?id=${product.id}">
-                <div class="card">
-                    <img src="${product.thumbnail}" alt="img">
-                    <div class="title">
-                        ${product.title}
-                    </div>
-                    <div class="price">
-                        $${product.price}
-                    </div>
-                </div>
-                </a>`
-            console.log(product)
+        products.push(...prod.products)
 
-        });
-        document.querySelector('.cards').innerHTML = str
-
+        renderProducts(products);
     }
     catch (error){
         console.log(`error found ${error}`)
     }
 }
 
-GetProducts()
+function renderProducts(list){
+    let str =``
+
+    list.forEach(product => {
+        str += `
+        <a href="./proddetails.html?id=${product.id}">
+        <div class="card">
+            <img src="${product.thumbnail}" alt="img">
+            <div class="title">
+                ${product.title}
+            </div>
+            <div class="price">
+                $${product.price}
+            </div>
+        </div>
+        </a>`;
+    });
+    document.querySelector('.cards').innerHTML = str;
+}
+
+document.querySelector('.search').addEventListener('input',e => {
+    const keyword = e.target.value.toLowerCase();
+
+    const filtered = products.filter(p => 
+        p.title.toLowerCase().includes(keyword)
+    );
+
+    renderProducts(filtered)
+});
+
+GetProducts();
