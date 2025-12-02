b975e3b01084a1ef2fbc09c7f2c88e4681b72352
anandhupp73 <anandhakrishnanpp73@gmail.com>
2025-12-02 13:06:31 +0530

    regex

---
+++ regex/test.js
@@ -0,0 +1,54 @@
+// let text = "Visit W3Schools!"; 
+// let n = text.search(/w3Schools/i);
+// console.log(n)
+
+// let text = 'hello world 100'
+// let pattern = /Hello/g
+// result = text.match(pattern)
+// console.log(result)
+
+// let text = 'hello world 100'
+// let pattern = /Hello/i
+// result = text.match(pattern)
+// console.log(result)
+
+// let text = 'hello world 100000'
+// let pattern = /0{2}/g
+// result = text.match(pattern)
+// // result = pattern.exec(text)
+// console.log(result)
+
+// let text = 'hello world 10076000'
+// let pattern = /[a-z]/g
+// result = text.match(pattern)
+// console.log(result)
+
+
+// let text = 'hello world 10076000'
+// let pattern = /^hello/g
+// result = text.match(pattern)
+// console.log(result)
+
+// let text = 'hello world 6000'
+// let pattern = /000$/g
+// result = pattern.test(text)
+// console.log(result)
+
+// let text = 'hello world 10076000'
+// let pattern = /hello(?! world)/g
+// result = text.match(pattern)
+// result = pattern.test(text)
+// console.log(result)
+
+// let text = 'hello world 10076000'
+// let pattern = /(?<=hello )world/g
+// result = text.match(pattern)
+// console.log(result)
+
+// let text = "Haha, haha, haha.";
+// const pattern = /(haha)+/;
+
+// let result = text.match(pattern);
+
+// console.log(result)
+
